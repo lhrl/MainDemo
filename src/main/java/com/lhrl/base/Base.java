ef8eb971c960948154c953ca4e62fb54fdf17a20
liulang <liulang@win7-PC>
2017-05-18 18:44:47 +0800

    Initial commit

---
+++ src/main/java/com/lhrl/base/Base.java
@@ -0,0 +1,10 @@
+package com.lhrl.base;
+
+/**
+ * Created by lhrl on 2017/5/18.
+ */
+public class Base {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
